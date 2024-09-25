a075f3d54c600f1449f9366c1c615142d610abc1
nick-bennett <nbennett10@cnm.edu>
2024-09-25 10:23:27 -0600

    assertThrows used instead of try-catch.

--- src/test/java/edu/cnm/deepdive/FizzBuzzTest.java
+++ src/test/java/edu/cnm/deepdive/FizzBuzzTest.java
@@ -3,18 +3,23 @@ package edu.cnm.deepdive;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 
 class FizzBuzzTest {
 
   @Test
   void valueOf_negativeInput() {
-    try {
-      FizzBuzz fb = new FizzBuzz();
-      fb.valueOf(-1);
-      fail();
-    } catch (IllegalArgumentException e) {
-      // Do nothing; exception is expected.
-    }
+    assertThrows(IllegalArgumentException.class, new NegativeFizzBuzzExecutable());
+  }
+
+}
+
+class NegativeFizzBuzzExecutable implements Executable {
+
+  @Override
+  public void execute() throws Throwable {
+    FizzBuzz fb = new FizzBuzz();
+    fb.valueOf(-1);
   }
 
 }
