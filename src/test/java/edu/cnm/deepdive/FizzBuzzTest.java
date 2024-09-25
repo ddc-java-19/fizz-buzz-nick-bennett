1c773406e9088ec5349cc7f0a49482f9f774d12e
nick-bennett <nbennett10@cnm.edu>
2024-09-25 11:19:14 -0600

    Change nested class to a local class.

--- src/test/java/edu/cnm/deepdive/FizzBuzzTest.java
+++ src/test/java/edu/cnm/deepdive/FizzBuzzTest.java
@@ -2,24 +2,26 @@ package edu.cnm.deepdive;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.function.Executable;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class FizzBuzzTest {
 
-  @Test
-  void valueOf_negativeInput() {
+  @ParameterizedTest
+  @ValueSource(ints = {-1, -2, -15, Integer.MIN_VALUE})
+  void valueOf_negativeInput(int value) {
+    class NegativeFizzBuzzExecutable implements Executable {
+
+      @Override
+      public void execute() throws Throwable {
+        FizzBuzz fb = new FizzBuzz();
+        fb.valueOf(value);
+      }
+
+    }
     assertThrows(IllegalArgumentException.class, new NegativeFizzBuzzExecutable());
   }
 
 }
 
-class NegativeFizzBuzzExecutable implements Executable {
-
-  @Override
-  public void execute() throws Throwable {
-    FizzBuzz fb = new FizzBuzz();
-    fb.valueOf(-1);
-  }
-
-}
