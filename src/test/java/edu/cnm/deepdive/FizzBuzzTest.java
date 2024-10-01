a7a1802f9f0a06e5e9de82319b439af8e330f604
nick-bennett <nbennett10@cnm.edu>
2024-10-01 10:14:11 -0600

    Updated tests to use FizzBuzz enum.

--- src/test/java/edu/cnm/deepdive/FizzBuzzTest.java
+++ src/test/java/edu/cnm/deepdive/FizzBuzzTest.java
@@ -2,6 +2,8 @@ package edu.cnm.deepdive;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.EnumSet;
+import java.util.Set;
 import org.junit.jupiter.api.function.Executable;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -11,31 +13,31 @@ class FizzBuzzTest {
   @ParameterizedTest
   @ValueSource(ints = {-1, -2, -15, Integer.MIN_VALUE})
   void valueOf_invalid(int value) {
-    assertThrows(IllegalArgumentException.class, () -> new FizzBuzz().valueOf(value));
+    assertThrows(IllegalArgumentException.class, () -> FizzBuzz.valueOf(value));
   }
 
   @ParameterizedTest
   @ValueSource(ints = {1, 11, Integer.MAX_VALUE, 97})
   void valueOf_neither(int value) {
-    assertEquals(String.valueOf(value), new FizzBuzz().valueOf(value));
+    assertEquals(Set.of(), FizzBuzz.valueOf(value));
   }
 
   @ParameterizedTest
   @ValueSource(ints = {3, 6, 99, Integer.MAX_VALUE - 4})
   void valueOf_fizz(int value) {
-    assertEquals(FizzBuzz.FIZZ_RESULT, new FizzBuzz().valueOf(value));
+    assertEquals(Set.of(FizzBuzz.FIZZ), FizzBuzz.valueOf(value));
   }
 
   @ParameterizedTest
   @ValueSource(ints = {55, 100, 250, Integer.MAX_VALUE - 2})
   void valueOf_buzz(int value) {
-    assertEquals(FizzBuzz.BUZZ_RESULT, new FizzBuzz().valueOf(value));
+    assertEquals(Set.of(FizzBuzz.BUZZ), FizzBuzz.valueOf(value));
   }
 
   @ParameterizedTest
   @ValueSource(ints = {0, 90, 255, Integer.MAX_VALUE - 7})
   void valueOf_fizzBuzz(int value) {
-    assertEquals(FizzBuzz.FIZZ_BUZZ_RESULT, new FizzBuzz().valueOf(value));
+    assertEquals(EnumSet.allOf(FizzBuzz.class), FizzBuzz.valueOf(value));
   }
 
 
